bbb182e5cdf6810d9e01fdc81a167ba9a2d1ea83
Priyadharshini Murugan <priyadharshini.murugan@relevantz.com>
2024-05-13 14:55:05 +0530

    Add project files.

---
+++ LXP.Common/ViewModels/RegisterUserViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LXP.Common.ViewModels
+{
+    public class RegisterUserViewModel
+    {
+
+        public string Email { get; set; } = null!;
+
+        public string? Password { get; set; }
+
+        public string Role { get; set; } = null!;
+
+        //public string CreatedBy { get; set; } = null!;
+
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public string? Dob { get; set; }
+
+        public string Gender { get; set; } = null!;
+
+        public string ContactNumber { get; set; } = null!;
+
+        public string Stream { get; set; } = null!;
+
+        
+
+        //public string? ProfilePhoto { get; set; }
+
+    }
+}
